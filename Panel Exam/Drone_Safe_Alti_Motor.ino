a5baa9f4329538eb4b01afec245fb2588710a787
kjdawson <kennethdawson@Kenneths-MacBook-Pro-2.local>
2016-11-24 19:03:59 -0500

    all sensors working with arduino
    all sensors working with arduino and the motor code.

--- Panel Exam/Drone_Safe_Alti_Motor.ino
+++ Panel Exam/Drone_Safe_Alti_Motor.ino
@@ -4,15 +4,38 @@
 #include <Servo.h>
 #include <Wire.h>
 #include <Adafruit_MPL3115A2.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
+
 
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 
 float prevaltm;
+float scale = 16384.0;
+float gyroscale = 131.0;
 float startaltm;
 int startaltmcount = 0;
 float altm;
 int altmcount = 0;
 int falling = 0;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+#define OUTPUT_READABLE_ACCELGYRO
 
 Servo esc;
 
@@ -21,10 +44,37 @@ int minPulseRate = 1000;
 int maxPulseRate = 2000;
 int throttleChangeDelay = 100;
 
+#define LED_PIN 13
+bool blinkState = false;
+
 void setup() {
   
   Serial.begin(9600);
   Serial.setTimeout(500);
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+
+  // initialize serial communication
+  // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+  // it's really up to you depending on your project)
+  //Serial.begin(38400);
+
+  
+  // set to +/- 8G sensitivity
+  accelgyro.setFullScaleAccelRange(2);
+
+  // initialize device
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+
+  // verify connection
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
   
   // Attach the the servo to the correct pin and set the pulse range
   esc.attach(escPin, minPulseRate, maxPulseRate); 
@@ -39,22 +89,43 @@ void loop() {
     return;
   }
 
-  altm = baro.getAltitude();
-  // Calculate rate of descent
-  if ((prevaltm - altm) > 0.5) {
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+      // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    Serial.print("Acc X ");
+    Serial.print(ax/scale);
+    Serial.print("Acc y ");
+    Serial.print(ay/scale);
+    Serial.print("Acc z ");
+    Serial.println(az/scale);
+    Serial.print("Gyro X ");
+    Serial.print(gx/gyroscale);
+    Serial.print("Gyro y ");
+    Serial.print(gy/gyroscale);
+    Serial.print("Gyro z ");
+    Serial.println(gz/gyroscale);
+    
+    altm = baro.getAltitude();
+    // Calculate rate of descent
+    if ((prevaltm - altm) > 0.75) {
       falling = 1;
     }
-  // Get starting altitude value
-  if (startaltmcount == 5) {
-    startaltm = altm;
     
-    }
-  
-  // Get previous altitude value
-  if (altmcount != 0) {
-     prevaltm = altm;
-     altmcount = 0;
-    }
+    // Get starting altitude value
+    if (startaltmcount == 5) {
+      startaltm = altm;
+      }
+    Serial.println("altitude ----------");
+    Serial.println(altm);
+    
+    // Get previous altitude value
+    if (altmcount != 0) {
+      prevaltm = altm;
+      altmcount = 0;
+      }
 
   // Wait for some input
   /*if (Serial.available() > 0 ) {
@@ -68,7 +139,7 @@ void loop() {
     Serial.print("Setting throttle to: ");
     Serial.println(throttle);
 */
-    int throttle = 20;
+    int throttle = 10;
     if (falling == 1) {
     // Change throttle to the new value
     // dummy throttle value
